3a92a7f5a776e11dab8764e0ab775696613c7800
Juanjomarg <juanj.martinezg96@gmail.com>
2023-01-03 21:42:50 -0500

    XML fixes

--- Python_files/RSS_puller_parser.py
+++ Python_files/RSS_puller_parser.py
@@ -7,38 +7,55 @@ from Python_files.functions import *
 #
 ###########################################################################
 
-def crawl_rss_feed(RSS_urls):
-    print('##############')
-    print('Obtanining XML')
-    print('##############\n')
-    
-    hoy=fecha_hoy()
+def get_all_rss(RSS_urls,date):
+    for key in RSS_urls.keys():
+        get_xml(key,RSS_urls.get(f"{key}"),date)
+
+def parse_all_rss(RSS_urls,date):
     for key in RSS_urls:
+        parse_xml(key,date)
 
-        print(f'Obtaining XML from {key}\n')
-        url=requests.get(RSS_urls[key])
-        print(f'Response was {url.status_code}')
+def get_xml(RSS_feed,RSS_feed_url,date):
+    print('###############')
+    print(' Obtaining XML')
+    print('###############\n')
+    
+    HEADERS = {
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+        }
 
-        print('###############')
-        print('  Making soup ')
-        print('###############\n')
+    print(f'Obtaining XML from {RSS_feed}')
+    try:
+        url=requests.get(RSS_feed_url,headers=HEADERS)
+        print(f'Response was {url.status_code}\n')
 
-        #"""
-        soup = BeautifulSoup(url.content,features="xml")
+    except Exception as e:
+        print(f'Error fetching {RSS_feed}')
+        print(e)
+
+    try:    
+        soup = BeautifulSoup(url.content, features="xml")
         pretty=soup.prettify()
-        #"""
-        
-        with open(f'Assets/RSS/RAW/XML/{hoy}-{key}.xml', 'wb',encoding="utf-8") as f:
-            f.write(url.content)
-            f.close
-        
-        """
-        f = open(fr"./Assets/RSS/RAW/XML/{hoy}-{key}.xml","w",encoding="utf-8")
-        f.write(pretty)
-        f.close
-        """
+        print('################')
+        print('   Making soup   ')
+        print('################\n')
+        print(f'Parsed XML file for {RSS_feed}\n')
+    except Exception as e:
+        print(f'Could not parse the XML for {RSS_feed}\n')
+        print(e)
 
-        print(f'XML generado para {key} con fecha de {hoy}\n')
+    print('###############')
+    print('  Saving file ')
+    print('###############\n')
+
+    plain=pretty
+    print(type(plain))
+    string=str(plain)
+
+    with open(f"./Assets/RSS/RAW/XML/{date}-{RSS_feed}.xml", 'w', encoding='UTF-8') as f:
+        f.write(string)
+
+    print(f'XML saved for {RSS_feed} for {date}\n')
 
 ###########################################################################
 #
@@ -46,27 +63,66 @@ def crawl_rss_feed(RSS_urls):
 #
 ###########################################################################
 
-def read_rss_xml_feed(RSS_urls):
-    print('\n#############################')
-    print('Extrayendo información de XML')
-    print('#############################\n')
+def parse_xml(RSS_feed,date):
 
-    hoy=fecha_hoy()
-    for key in RSS_urls:
-        print(f'Intentando cargar contenido XML para {key}')
-        infile = open(fr"./Assets/RSS/RAW/XML/{hoy}-{key}.xml","r",encoding="utf-8")
-        contents = infile.read()
-        parsed=feedparser.parse(contents)
-        print(f'Contenido obtenido, Parsing...')
+    print(f'Trying to load XML file for {RSS_feed}')
+    try:
+        file = open(fr"./Assets/RSS/RAW/XML/{date}-{RSS_feed}.xml","r",encoding="utf-8")
+        print('################')
+        print('   Loading XML   ')
+        print('################\n')
+        print(f"Loaded XML file for {RSS_feed}\n")
+    except Exception as e:
+        print(f"Couldn't load XML file for {RSS_feed} from disk")
 
-        posts = []
-        for post in parsed.entries:
-            posts.append((post.title, post.link, post.description))
-        print(f'Intentando crear CSV para {key}')
+    contents = file.read()
 
-        df = pd.DataFrame(posts, columns=['title', 'link', 'description'])
-        df.to_csv(fr"./Assets/RSS/RAW/CSV/{hoy}-{key}.csv",index=False)
-        print(f'CSV creado exitosamente para {key} con fecha de {hoy}\n')
+    try:    
+        soup = BeautifulSoup(contents, features="xml")
+        print('################')
+        print('   Making soup   ')
+        print('################\n')
+        print(f'Parsed XML file for {RSS_feed}\n')
+    except Exception as e:
+        print(f'Could not parse the XML for {RSS_feed}\n')
+        print(e)
+        
+    entries=soup.find_all("item")
+
+    titles=[]
+    descriptions=[]
+    links=[]
+    pubDates=[]
+
+    print('##############')
+    print('   Sorting   ')
+    print('##############\n')
+    
+    for i in entries:
+        title=i.find("title").text.replace('\n', '').replace('\t', '').replace('  ', '').replace('   ', '').replace('    ', '').replace('´', "'").replace('’', "'").replace('–', "-").replace('‘', "'").replace('с', "c")
+        titles.append(title)
+
+        description=i.find("description").text.replace('\n', '').replace('\t', '').replace('  ', '').replace('   ', '').replace('    ', '').replace('´', "'").replace('’', "'").replace('–', "-").replace('‘', "'").replace('с', "c")
+        descriptions.append(description)
+
+        link=i.find("link").text.replace('\n', '').replace('\t', '').replace('  ', '').replace('   ', '').replace('    ', '')
+        links.append(link)
+
+        pubDate=i.find("pubDate").text.replace('\n', '').replace('\t', '').replace('  ', '').replace('   ', '').replace('    ', '')
+        pubDates.append(pubDate)
+
+    print(f'Sorted news for {RSS_feed} for {date}\n')
+    
+    print('############')
+    print('   Saving   ')
+    print('############\n')
+
+    data={"titles":titles,"descriptions":descriptions,"links":links,"pubDates":pubDates}
+
+    df=pd.DataFrame(data=data)
+    df.to_csv(fr"./Assets/RSS/RAW/CSV/{date}-{RSS_feed}.csv",index=False)
+
+    print(f'CSV saved for {RSS_feed} for {date}\n')
 
 ###########################################################################
 #
@@ -75,9 +131,9 @@ def read_rss_xml_feed(RSS_urls):
 ###########################################################################
 
 def combine_news_of_day(fecha_aggregate):
-    print('\n##########################################')
+    print('##########################################')
     print(f'Combinando CSV del día {fecha_aggregate}')
-    print('#########################################\n')
+    print('##########################################\n')
 
     #Nombres de archivos CSV desde carpeta   
     files=glob.glob(fr"./Assets/RSS/RAW/CSV/{fecha_aggregate}*.csv")
@@ -150,12 +206,10 @@ RSS_urls = json.load(open(fr"./Assets/RSS/urls.json"))
 
 def actualizar_noticias_a_hoy():
 
-    crawl_rss_feed(RSS_urls)
-    read_rss_xml_feed(RSS_urls)
-
-    #Fecha aggregate espera fecha en formato dia-mes-año
-    #Se puede usar fecha_hoy() para obtener fecha actual
     fecha_aggregate=fecha_hoy()
+
+    get_all_rss(RSS_urls,fecha_aggregate)
+    parse_all_rss(RSS_urls,fecha_aggregate)
 
     combine_news_of_day(fecha_aggregate)
     aggregate_news()
